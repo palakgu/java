6032381f27507770a433bfe5092605701071baad
palak gupta <palakofficial21@gmail.com>
2025-01-07 12:46:02 +0530

    division

--- calc.java
+++ calc.java
@@ -2,6 +2,28 @@ class Calculator {
 public static void main(String args[]){
 System.out.println("Welcome to the calculator");
 
+Scanner sc = new scanner(System.in);
+System.out.println("enter the numbers")
+int n = sc.nextInt();
+int m = sc.nextInt();
+System.out.println("enter the operation");
+char sign = sc.nextInt.charAt(0);
+int res;
+switch(sign){
+
+case '+' :  res = addition(n,m);
+            System.out.println(res);
+ 
+case '-' : res = subtraction(n,m);
+            
+
+case '*' : multiplication(n,m);
+
+case '/' : division(n,m);
+
+
+}
+
 
 }
 
@@ -15,4 +37,8 @@ return a-b;
 public static int multiplication(int a , int b){
 return a*b;
 }
+public static float division(int a , int b){
+  return a/b;
+}
+
 }
